128e5d623d6df5fa44a7634de25ee7baaf4de0c9
shahsevil <shahsevil@gmail.com>
2020-06-28 04:21:43 +0400

    -- structure created -- repositories added

---
+++ src/main/java/app/repo/CurrencyRepo.java
@@ -0,0 +1,9 @@
+package app.repo;
+
+import app.entity.Currency;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurrencyRepo extends JpaRepository<Currency, Integer> {
+}
