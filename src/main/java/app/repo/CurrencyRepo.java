3278b645729dd21c9c107a95a70fdf46b13ce30d
Elvin <elvintaghiyev184@gmail.com>
2020-07-01 22:57:55 +0400

    remote db added

--- src/main/java/app/repo/CurrencyRepo.java
+++ src/main/java/app/repo/CurrencyRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CurrencyRepo extends JpaRepository<Currency, Integer> {
+public interface CurrencyRepo extends JpaRepository<Currency, Long> {
 }
