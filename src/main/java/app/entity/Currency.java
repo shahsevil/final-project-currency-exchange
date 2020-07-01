bfdd917e7ed664e4398ea334ce52092b27c04175
shahsevil <shahsevil@gmail.com>
2020-07-01 19:05:07 +0400

    -- entities and relations added

--- src/main/java/app/entity/Currency.java
+++ src/main/java/app/entity/Currency.java
@@ -1,4 +1,33 @@
 package app.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "currency")
 public class Currency {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "curr_id")
+    private long id;
+
+    @Column(name = "curr_name_abv")
+    private String abvName;
+
+    @Column(name = "curr_name_long")
+    private String longName;
+
+    @JsonIgnore
+    @ManyToMany(cascade = CascadeType.ALL,
+            mappedBy = "currencies")
+    private List<History> histories;
 }
--- src/main/java/app/entity/User.java
+++ src/main/java/app/entity/User.java
@@ -1,21 +1,31 @@
 package app.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Collection;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "user")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "u_id")
     private long id;
+
+    @Column(name = "u_full-name")
     private String full_name;
+
+    @Column(name = "u_email")
     private String email;
+
+    @Column(name = "password")
     private String password;
 
     public User(String full_name, String email, String password) {
@@ -23,4 +33,9 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private Collection<History> histories;
+
 }
