7bfbae8ac469be678ff77f7ac9baae4cee6fb41e
Elvin <elvintaghiyev184@gmail.com>
2020-07-15 16:28:08 +0400

    finished

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,11 @@
-insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('EUR','European Union', '€')
-insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('USD','American dollar','$')
-insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('DKK','Danish Krone','Kr.')
-insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('HUF','Hungarian Forint','Ft')
-insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('ISK','Icelandic Króna','kr')
-insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('BGN','Bulgarian Lev','лв.')
-insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('CNY','Chinese Yuan','¥')
-insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('PHP','Philippine Peso','₱')
-insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('GBP','British Pound Sterling','£')
-insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('RUB','Russian Ruble','RUB')
-INSERT INTO users (u_email, u_full_name, password) VALUES ('elvintaghiyev184@gmail.com', 'Elvin Taghizade', '123')
+insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('EUR','European Union', '€');
+insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('USD','American dollar','$');
+insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('DKK','Danish Krone','Kr.');
+insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('HUF','Hungarian Forint','Ft');
+insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('ISK','Icelandic Króna','kr');
+insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('BGN','Bulgarian Lev','лв.');
+insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('CNY','Chinese Yuan','¥');
+insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('PHP','Philippine Peso','₱');
+insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('GBP','British Pound Sterling','£');
+insert into currency (curr_name_abv, curr_name_long, curr_symbol) VALUES ('RUB','Russian Ruble','RUB');
+INSERT INTO users (u_email, u_full_name, password) VALUES ('test@test.com', 'Test', 'Qwerty123');
