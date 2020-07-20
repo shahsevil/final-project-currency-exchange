e5b595cd96bbc850d7cf4a9e48252b82e2436212
shahsevil <shahsevil@gmail.com>
2020-07-21 01:31:13 +0400

    -- polished a bit

--- src/main/java/app/controller/LandingController.java
+++ src/main/java/app/controller/LandingController.java
@@ -1,12 +1,15 @@
 package app.controller;
 
+import app.exception.WrongActionException;
 import app.security.XUserDetails;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Log4j2
 @Controller
@@ -20,4 +23,14 @@ public class LandingController {
         model.addAttribute("username", user.getUsername());
         return "landing";
     }
+
+    @PostMapping
+    public String handle_landing_post(@RequestParam(value = "get_started", required = false) String btn){
+        if ("get_started".equals(btn)) {
+            log.info("Redirect -> /authorized");
+            return "redirect:/authorized";
+        }
+        throw new WrongActionException();
+    }
+
 }
